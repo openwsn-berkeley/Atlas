2baec357b8b23cbe3a8a0b9aa83559bf44be42ac
Raa37 <48555638+Raa37@users.noreply.github.com>
2020-03-27 22:22:29 +0100

    #6

--- HNOO.py
+++ HNOO.py
@@ -7,8 +7,53 @@ user has option to either give a specific grid through a config file, or to have
 import json
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 #============================ helper functions ================================
+'''
+generate grid with given rows x coloums and randon obstacles
+'''
+def genRandGrid(rows,cols):
+    print(rows, cols)
+    randGrid=np.random.randint(2, size=(rows, cols))
+    print(randGrid)
+    return randGrid
+'''
+performs online obstacle avoidance and returns the path found 
+'''  
+def ObstacleAvoidAlg(start, target,grid):
+    
+    currentNode = start
+    onlinePath = [start]
+    
+    while (currentNode != target):
+        x=currentNode[0]
+        y=currentNode[1]
+        neighbourNodes = [(x+1,y),(x+1,y+1),(x,y+1),(x-1,y+1),(x-1,y),(x-1,y-1),(x,y-1),(x+1,y-1)]
+        avaliableNext=[]
+        print(neighbourNodes)
+        for node in neighbourNodes:
+            print("node = ", node)
+            NN=node
+            #print(NN[0],NN[1])
+            #print(grid[NN[0]][NN[1]])
+            if (NN[0])>=0 and (NN[1])>=0 and (grid[NN[0]][NN[1]])==1:
+                if(node != (0,0)) and (node in onlinePath) == False:
+                    idx=neighbourNodes.index(node)
+                    print("index =" , idx)
+                    avaliableNext.append(node)        
+        print(avaliableNext)
+        if avaliableNext==[]:
+            return print("no path avaliable")
+        else:
+            moveTo = avaliableNext[random.randint(0,len(avaliableNext)-1)]
+            print(moveTo)
+            onlinePath.append(moveTo)
+            print("online path so far is ", onlinePath)
+            currentNode=moveTo
+    
+    print(onlinePath)
+    return onlinePath
 
 '''
 calculates steps taken from source to destination
@@ -29,7 +74,12 @@ def main():
     targets    = [(5,6),(7,8)]     # FIXME
     numRuns    = 100
     navAlg     = None              # FIXME
-    
+    rows = random.randint(5,21)
+    cols = random.randint(5,21)
+    grid = genRandGrid(rows,cols)
+    start=(0,0)
+    target=(5,5)
+    ObstacleAvoidAlg(start,target,grid)
     # run all simulations
     with open('HNOO.log','w') as f:
         for grid in grids:
@@ -69,8 +119,8 @@ def main():
     plt.ylim(0, 0.03)
     plt.grid(True)
     '''
-    #plt.show()
-    plt.savefig('HNOO.png')
+   # plt.show()
+   # plt.savefig('HNOO.png')
     
     
     print('Done.')
