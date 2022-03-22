b6aed805ddaf5a4f46266ff0f2838967ebfdbd81
Raa37 <razanne_abu-aisheh@hotmail.com>
2022-03-22 11:53:42 +0100

    debugging

--- RunOneSim.py
+++ RunOneSim.py
@@ -106,14 +106,13 @@ def main(simSetting, simUI):
     start_time = time.time()
     base_dir = "./logs"
     os.makedirs(base_dir, exist_ok=True)
-
+    print(simSetting)
     unique_id = simSetting['seed']
     config_id = simSetting['config ID']
     log_file = f'{config_id}_{time.strftime("%y%m%d%H%M%S", time.localtime(start_time))}_{unique_id}.json'
     # for debugging
     # log
     config_data = simSetting
-    print(config_data)
     config_data["type"] = "sim configuration"
     logger.setFileName(os.path.join(base_dir, log_file))
     logger.log(config_data)
