8d5e13283684ef3599077abeb34e881d98875345
Raa37 <razanne_abu-aisheh@hotmail.com>
2022-03-22 16:19:26 +0100

    fixed runonesim args

--- RunOneSim.py
+++ RunOneSim.py
@@ -137,8 +137,8 @@ def main(simSetting, simUI):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--simSetting", type=str, help="simulation configuration settings as dictionary")
-    parser.add_argument("--simUI", type=str, help="UI configurations")
+    parser.add_argument("--simSetting", help="simulation configuration settings as dictionary")
+    parser.add_argument("--simUI", help="UI configurations")
 
     args = parser.parse_args()
 
