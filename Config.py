f61c93ed85899c0b6b3c3633e1cb71b26362f2d9
Raa37 <razanne_abu-aisheh@hotmail.com>
2022-04-27 13:03:51 +0200

    add config.py

---
+++ Config.py
@@ -0,0 +1,34 @@
+import pkg_resources
+import toml
+import threading
+
+class BaseConfig(dict):
+    """Thread-Safe Base Configuration Object"""
+    lock = threading.Lock()
+
+    def __init__(self, data):
+        if isinstance(data, str):
+            _dict = toml.load(data)
+        elif isinstance(data, dict):
+            _dict = data
+        else:
+            raise ValueError("Bad data type.")
+        super().__init__(_dict)
+
+    def __getattr__(self, *args):
+        with self.lock:
+            val = dict.get(self, *args)
+            return BaseConfig(val) if type(val) is dict else val
+
+    def __setattr__(self, *args, **kwargs):
+        with self.lock:
+            dict.__setitem__(*args, **kwargs)
+
+    def __delattr__(self, *args, **kwargs):
+        with self.lock:
+            dict.__delitem__(*args, **kwargs)
+
+class AtlasConfig(BaseConfig):
+    def __init__(self, config="default", base=None):
+        configpath = pkg_resources.resource_filename(__name__, f"configs/{config}.toml")
+        super().__init__(configpath)
