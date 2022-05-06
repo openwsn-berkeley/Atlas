886520ccb1b83127e7f0a63832e0d8aef1f5872a
Raa37 <razanne_abu-aisheh@hotmail.com>
2022-05-06 12:54:26 +0200

    #156. WIP

--- Orchestrator.py
+++ Orchestrator.py
@@ -26,7 +26,7 @@ class Orchestrator(Wireless.WirelessDevice):
     '''
     
     COMM_DOWNSTREAM_PERIOD_S    = 1
-    MINFEATURESIZE_M            = 1
+    MINFEATURESIZE              = 1
     
     def __init__(self, numRobots, initX, initY):
 
@@ -121,10 +121,70 @@ class Orchestrator(Wireless.WirelessDevice):
             speed    = dotbot['speed'],
             duration = frame['duration'],
         )
-
+        self.hCellsObstacle += [self._xy2hCell(newX, newY)]
+        cellsTraversed = self._cellsTraversed(0, 0, 5, 5)
+        log.debug(f'cells traversed -> {cellsTraversed}')
+        self.hCellsOpen += [c for c in cellsTraversed]
+        log.debug(f'open cells -> {self.hCellsOpen}')
         dotbot['x']  = newX
         dotbot['y']  = newY
         log.debug(f'dotbot {dotbot} is at ( {newX},{newY} ) ')
+
+    #=== Map
+
+    def _cellsTraversed(self, startX, startY, stopX, stopY):
+        returnVal = []
+        log.debug(f"dotbot moved from ({startX},{startY}) to ({stopX},{stopY})")
+        # scan horizontally
+        x = startX
+        while True:
+
+            if startX < stopX:
+                # going right
+                x += self.MINFEATURESIZE / 2
+                if x > stopX:
+                    break
+            else:
+                # going left
+                x -= self.MINFEATURESIZE / 2
+                if x < stopX:
+                    break
+
+            y = startY + (((stopY - startY) * (x - startX)) / (stopX - startX))
+            (cx, cy) = self._xy2hCell(x, y)
+            returnVal += [(cx, cy)]
+
+        # scan vertically
+        y = startY
+        while True:
+
+            if startY < stopY:
+                # going down
+                y += self.MINFEATURESIZE / 2
+                if y > stopY:
+                    break
+            else:
+                y -= self.MINFEATURESIZE / 2
+                if y < stopY:
+                    break
+
+            x = startX + (((stopX - startX) * (y - startY)) / (stopY - startY))
+            (cx, cy) = self._xy2hCell(x, y)
+            returnVal += [(cx, cy)]
+
+        # filter duplicates
+        returnVal = list(set(returnVal))
+        log.debug(f'new cells found -> {returnVal}')
+        return returnVal
+
+    def _xy2hCell(self, x, y):
+
+        xsteps = int(round((x - self.initX) / (self.MINFEATURESIZE / 2), 0))
+        cx = self.initX + xsteps * (self.MINFEATURESIZE / 2)
+        ysteps = int(round((y - self.initY) / (self.MINFEATURESIZE / 2), 0))
+        cy = self.initY + ysteps * (self.MINFEATURESIZE / 2)
+
+        return (cx, cy)
 
     #=== UI
 
