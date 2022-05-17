679d4372ed5146c84b4bb08e9044676d6113e2ee
Raa37 <razanne_abu-aisheh@hotmail.com>
2022-05-17 16:10:53 +0200

    #181. isMapValid() works, unit tests pass, need to integrate into simulator

--- Floorplan.py
+++ Floorplan.py
@@ -25,7 +25,7 @@ class Floorplan(object):
             'height':    self.height,
             'obstacles': self.obstacles,
         }
-    
+
     #======================== static =========================================
 
     @staticmethod
@@ -40,3 +40,68 @@ class Floorplan(object):
                     obstacles += [{'x': x, 'y':  y, 'width': 1, 'height': 1}] # TODO: could do line merging here to
         return width, height, obstacles
 
+    def _isMapValid(self, drawing):
+        '''
+        checks if map given to floorplan is valid
+        aka. has borders and valid characters (#,., s)
+        example input - output :
+            {
+            'in': {
+                'drawing':
+                    \'''
+                    ########
+                    #...s..#
+                    ########
+                    \'''
+            },
+            'out': False
+        },
+        '''
+
+        returnVal       = True
+
+        # split drawing into matrix of characters
+        matrixOfChars   = [line for line in drawing.splitlines() if line]
+        w = max([len(row) for row in matrixOfChars])
+        h = len(matrixOfChars)
+
+        charsChecked    = []     # character positions in map of which surrounding frontiers have been determined
+        charsToCheck    = []     # character positions in map of which surrounding frontiers are to be determined
+
+        # if there is no starting position character 's' map is invalid
+        try:
+            startPos    = [(lidx,line.index('s')) for (lidx,line) in enumerate(matrixOfChars) if 's' in line][0]
+        except IndexError:
+            startPos    = None
+            returnVal   = False
+
+        charsToCheck   += [startPos]
+
+        # keep looping until there are no more froniters (all borders have been found)
+        while charsToCheck and startPos:
+
+            (charRow,charCol) = charsToCheck.pop(0)
+            charsChecked     += [(charRow,charCol)]
+
+            # set surrounding 8 character positions as frontiers to check next if they are not obstacles
+            neighbourChars    = [
+                (charRow+1,  charCol  ), (charRow-1,  charCol  ),
+                (charRow,    charCol-1), (charRow,    charCol+1),
+                (charRow-1,  charCol-1), (charRow+1,  charCol+1),
+                (charRow-1,  charCol+1), (charRow+1,  charCol-1)
+            ]
+
+            for nx, ny in neighbourChars:
+
+                try:
+                    # if drawing contains characters that aren't '#,.,s' map is invalid
+                    assert matrixOfChars[nx][ny] == '#' or matrixOfChars[nx][ny] == '.' or matrixOfChars[nx][ny] == 's'
+
+                    # if next frontier position is not in matrix of characters map is invalid
+                    if matrixOfChars[nx][ny] != '#' and (nx, ny) not in charsToCheck  and (nx, ny) not in charsChecked:
+                        charsToCheck += [(nx, ny)]
+
+                except IndexError:
+                    returnVal = False
+
+        return returnVal
---
+++ tests/test_isMapValid.py
@@ -0,0 +1,96 @@
+import Floorplan
+import pytest
+
+# ============================ fixtures ==============================
+
+EXPECTEDINOUT = [
+
+    # full border
+    {
+        'in': {
+            'drawing':
+                '''
+                ######
+                #....#
+                #s...#
+                ######
+                '''
+        },
+        'out': True
+    },
+
+    # incomplete border
+    {
+        'in': {
+            'drawing':
+                '''
+                ######
+                #....#
+                #s....
+                ######
+                '''
+        },
+        'out': False
+    },
+
+    # border not exact square or rectangle
+    {
+        'in': {
+            'drawing':
+                '''
+                ######...
+                #....####
+                #...s...#
+                #########
+                '''
+        },
+        'out': True
+    },
+
+    # missing starting position
+    {
+        'in': {
+            'drawing':
+                '''
+                ######
+                #....#
+                #.....
+                ######
+                '''
+        },
+        'out': False
+    },
+
+    # invalid character b
+    {
+        'in': {
+            'drawing':
+                '''
+                ######
+                #....#
+                #..b.#
+                ######
+                '''
+        },
+        'out': False
+    },
+
+]
+
+@pytest.fixture(params=EXPECTEDINOUT)
+def expectedInOut(request):
+    return request.param
+
+# ============================ tests =================================
+
+def test_isMapValid(expectedInOut):
+    '''
+    testing if map given to floorplan is valid
+    aka. has borders and valid characters (#,., s)
+    '''
+
+    floorplan = Floorplan.Floorplan(expectedInOut['in']['drawing'])
+
+    assert floorplan._isMapValid(*expectedInOut['in'].values()) == expectedInOut['out']
+
+
