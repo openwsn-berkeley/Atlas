0ca05d586790900472d2f25015e817abe80d1f3a
Raa37 <razanne_abu-aisheh@hotmail.com>
2022-06-02 14:40:04 +0200

    #214. test cleanup

--- tests/test_getPDR.py
+++ tests/test_getPDR.py
@@ -6,9 +6,18 @@ import pytest
 EXPECTEDINOUT = [
 
     {
+        'linkStabilities': {
+            ('A', 'B'): 0.60,
+            ('A', 'C'): 0.90,
+            ('A', 'D'): 0.20,
+            ('B', 'D'): 0.70,
+            ('C', 'D'): 0.80,
+        },
+
         'in': {
-            'sender':      'A',
-            'receiver':    'D',
+
+            'sender':       'A',
+            'receiver':     'D',
             'relays':      ['B', 'C'],
         },
         'out': 0.87008
@@ -22,23 +31,15 @@ def expectedInOut(request):
 # ============================ tests =================================
 
 class TestWireless(Wireless.Wireless):
-    def __new__(cls):
+    def __new__(cls, linkStabilities):
         return object.__new__(cls)
 
+    def __init__(self, linkStabilities):
+        super().__init__()
+        self.linkStabilities = linkStabilities
+
     def _getStability(self, sender, receiver):
-        pdrs = {
-            ('A','B'): 0.60,
-            ('A','C'): 0.90,
-            ('A','D'): 0.20,
-            ('B','A'): 0.60,
-            ('B','D'): 0.70,
-            ('C','A'): 0.90,
-            ('C','D'): 0.80,
-            ('D','A'): 0.20,
-            ('D','B'): 0.70,
-            ('D','C'): 0.80
-        }
-        return pdrs[(sender, receiver)]
+        return self.linkStabilities[(sender, receiver)]
 
 def test_getPDR(expectedInOut):
     '''
@@ -46,7 +47,7 @@ def test_getPDR(expectedInOut):
     root node given relays and link PDRs
     '''
 
-    testwireless = TestWireless()
+    testwireless = TestWireless(expectedInOut['linkStabilities'])
 
     assert testwireless._getPDR(*expectedInOut['in'].values()) == expectedInOut['out']
-    
+
